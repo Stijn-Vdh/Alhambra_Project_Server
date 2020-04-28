c1e6cb7f47e27d79ad417d50ae8e998c7a4188c7
cirdan.de.smedt <cirdan.de.smedt@student.howest.ne>
2020-04-28 11:04:26 +0200

    created class Game and a GameRepo to create a new game pair work cirdan en stav

--- src/main/java/be/howest/ti/alhambra/logic/Game.java
+++ src/main/java/be/howest/ti/alhambra/logic/Game.java
@@ -9,16 +9,46 @@ public class Game {
     private Set<Player> players = new HashSet<>();
     private int playerCount;
     private int readyCount;
-    private boolean gameStarted = false;
-
-    public void add(Player player) {players.add(player);}
+    private boolean gameStarted;
 
 
 
-
-    public Game(String gameID, Set<Player> players) {
+    public Game(String gameID, String name) {
         this.gameID = gameID;
-        this.players = players;
+        Player player = new Player(name, false);
+        addPlayer(player);
+        this.playerCount = getPlayerCount();
+        this.readyCount = getReadyCount();
+        this.gameStarted = isGameStarted();
+    }
 
+    public int getPlayerCount() {
+        playerCount = players.size();
+        return playerCount;
+    }
+
+    public int getReadyCount() {
+        int counter = 0;
+        for (Player player: players) {
+            if (player.getReady()){
+                counter++;
+            }
+        }
+        readyCount = counter;
+        return readyCount;
+    }
+
+    public void addPlayer(Player player) {players.add(player);}
+
+    public Set<Player> getPlayers() {return players;}
+
+
+    public boolean isGameStarted() {
+        gameStarted = readyCount == playerCount;
+        return gameStarted;
+    }
+
+    public String getGameID() {
+        return gameID;
     }
 }
---
+++ src/main/java/be/howest/ti/alhambra/logic/GameRepo.java
@@ -0,0 +1,36 @@
+package be.howest.ti.alhambra.logic;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GameRepo {
+
+    private Set<Game> games = new HashSet<>();
+
+
+    public GameRepo() {
+        newGame();
+    }
+
+    public String generateGameID(){
+        String group = "group01-";
+        int counter;
+        if (games.isEmpty()){
+            counter = 1;
+        } else{
+            counter = games.size() + 1;
+        }
+
+        return group + counter;
+    }
+
+    public void newGame(){
+        Game game = new Game(generateGameID(), getPlayerName());
+        games.add(game);
+    }
+
+    private String getPlayerName() {
+        return null;
+    }
+
+}

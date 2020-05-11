32ea12c638434b987a25e8560425c2e868c70c9e
stav.heuvinck <stav.heuvinck@student.howest.be>
2020-05-11 16:48:00 +0200

    added more tests Pair work Cirdan & Stav

--- src/test/java/be/howest/ti/alhambra/logic/CityTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/CityTest.java
@@ -2,7 +2,11 @@ package be.howest.ti.alhambra.logic;
 
 
 import be.howest.ti.alhambra.logic.building.Building;
+import be.howest.ti.alhambra.logic.building.BuildingType;
+import be.howest.ti.alhambra.logic.building.Walls;
+import be.howest.ti.alhambra.logic.exceptions.AlhambraGameRuleException;
 import be.howest.ti.alhambra.logic.game.City;
+import be.howest.ti.alhambra.logic.game.Location;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,12 +18,23 @@ public class CityTest {
     void city(){
 
         City c = new City();
+        Building a = new Building(BuildingType.ARCADES, 2, new Walls(true,false,true,false));
+        Building p = new Building(BuildingType.PAVILION, 2, new Walls(true,false,true,false));
 
         assertNull(c.getBoard()[1][1]);
-        assertEquals(0, c.getBoard()[0][0].getCost());
+        assertEquals(0, c.getBoard()[3][3].getCost());
 
-        assertTrue(c.hasNeighbours(0,1));
+        assertThrows(AlhambraGameRuleException.class, () -> c.placeBuiding(a, new Location(5,5)));
+
+        c.placeBuiding(p, new Location(3,4));
+        assertEquals(BuildingType.PAVILION,c.getBoard()[3][4].getType());
+
+        assertThrows(AlhambraGameRuleException.class, () -> c.placeBuiding(p, new Location(3,4)));
+
+        assertTrue(c.hasNeighbours(3,4));
         assertFalse(c.hasNeighbours(1,1));
+
+
     }
 
 }

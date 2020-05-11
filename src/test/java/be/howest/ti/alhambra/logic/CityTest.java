739347fe64972a33b543c2abef28227564c5ca20
stav.heuvinck <stav.heuvinck@student.howest.be>
2020-05-11 14:28:04 +0200

    add tests for the function hasNeighbours Pair work Cirdan & Stav

--- src/test/java/be/howest/ti/alhambra/logic/CityTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/CityTest.java
@@ -1,6 +1,7 @@
 package be.howest.ti.alhambra.logic;
 
 
+import be.howest.ti.alhambra.logic.building.Building;
 import be.howest.ti.alhambra.logic.game.City;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,8 @@ public class CityTest {
         assertNull(c.getBoard()[1][1]);
         assertEquals(0, c.getBoard()[0][0].getCost());
 
+        assertTrue(c.hasNeighbours(0,1));
+        assertFalse(c.hasNeighbours(1,1));
     }
 
 }

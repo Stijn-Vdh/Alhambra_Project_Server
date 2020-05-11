fcdf949fa0f266a360bf4b3ff60e9355f59dc3df
Daniel Vlaeminck <daniel.vlaeminck@student.howest.be>
2020-05-11 11:47:18 +0200

    renamed coinsInBag to coins renamed computeCurrencyValue to computeSelectedCoinsValue added removeSelectedCoin and removeSelectedCoinsFromBag methods

--- src/main/java/be/howest/ti/alhambra/logic/player/CoinBag.java
+++ src/main/java/be/howest/ti/alhambra/logic/player/CoinBag.java
@@ -9,41 +9,51 @@ import java.util.List;
 
 public class CoinBag {
 
-    private List<Coin> coinsInBag = new ArrayList<>();
+    private List<Coin> coins = new ArrayList<>();
     private List<Coin> selectedCoins = new ArrayList<>();
 
+
     public void addCoin(Coin coin){
-        coinsInBag.add(coin);
+        coins.add(coin);
     }
 
     public void addSelectedCoin(Coin coin){
         selectedCoins.add(coin);
     }
 
-    public int computeTotalValue(){
+    public void removeSelectedCoin(Coin coin){
+        selectedCoins.remove(coin);
+    }
+
+    public void removeSelectedCoinsFromBag(){
+        for (Coin coin: selectedCoins){
+            coins.remove(coin);
+        }
+    }
+
+    public int computeTotalCoinsValue(){
         int totalValue = 0;
 
-        for (Coin coin: coinsInBag){
+        for (Coin coin: coins){
             totalValue += coin.getAmount();
         }
 
         return totalValue;
     }
-    
 
-    public int computeCurrencyValue(){
+    public int computeSelectedCoinsValue(){
         Currency color = selectedCoins.get(0).getCurrency();
-        int value = 0;
+        int selectedCoinsValue = 0;
 
-        for (Coin coin: coinsInBag){
+        for (Coin coin: coins){
             if (!(coin.getCurrency().equals(color))){
-                throw new AlhambraGameRuleException("Different currencies are now allowed");
+                throw new AlhambraGameRuleException("Different currencies are not allowed");
             }else{
-                value += coin.getAmount();
+                selectedCoinsValue += coin.getAmount();
             }
         }
 
-        return value;
+        return selectedCoinsValue;
     }
 
 

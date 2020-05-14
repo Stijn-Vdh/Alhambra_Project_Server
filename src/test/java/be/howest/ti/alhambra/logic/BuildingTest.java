e760065e00525219ab4c90723526a1c03bd4d40c
stav.heuvinck <stav.heuvinck@student.howest.be>
2020-05-14 23:44:26 +0200

    added test for the placeBuilding

--- src/test/java/be/howest/ti/alhambra/logic/BuildingTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/BuildingTest.java
@@ -42,7 +42,7 @@ public class BuildingTest {
 
     @Test
     void placeBuilding(){
-        
+
         AlhambraController controller = new AlhambraController();
 
         controller.initializeLobby();

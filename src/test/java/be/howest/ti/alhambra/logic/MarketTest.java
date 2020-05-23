7599facb8df56346eebdc84751471723eca552f7
cirdan.de.smedt <cirdan.de.smedt@student.howest.ne>
2020-05-23 16:57:46 +0200

    removed double testcode

--- src/test/java/be/howest/ti/alhambra/logic/MarketTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/MarketTest.java
@@ -65,36 +65,5 @@ public class MarketTest {
         assertThrows(AlhambraGameRuleException.class, ()-> market.buyBuilding(player, Currency.GREEN, coins2));
 
     }
-
-    @Test
-    void BuyBuilding(){
-        List<Coin> coins = new LinkedList<>();
-        Queue<Building> buildings = new LinkedList<>();
-        Walls walls = new Walls(true, false, true, false);
-        Player player = new Player("john");
-
-        for (int i = 3; i < 9; i++){
-            buildings.add(new Building(BuildingType.PAVILION, i, walls));
-        }
-
-        for (int i = 0; i < 1; i++) {
-            coins.add(new Coin(Currency.GREEN, i));
-        }
-        Market market = new Market(buildings);
-
-        assertEquals(4, market.getBuildingsOnBoard().size());
-        assertEquals(2, market.getAmountOfBuildings());
-
-        assertThrows(AlhambraGameRuleException.class, ()-> market.buyBuilding(player, Currency.GREEN, coins));
-        for (int i = 0; i < 5; i++) {
-            coins.add(new Coin(Currency.GREEN, i+2));
-        }
-
-        assertThrows(AlhambraGameRuleException.class, ()-> market.buyBuilding(player, Currency.BLUE, coins));
-        assertTrue(player.getBuildingsInHand().isEmpty());
-        market.buyBuilding(player,Currency.GREEN, coins);
-
-        assertFalse(player.getBuildingsInHand().isEmpty());
-
-    }
+    
 }
